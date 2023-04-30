37014fcc9eac63c3f62e7507792c831b11c0b633
mohammadne <mohammadne.dev@gmail.com>
2023-04-30 16:19:09 +0330

    feat: add cmd

---
+++ cmd/server.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/CafeKetab/user-go/internal/config"
+	"github.com/CafeKetab/user-go/pkg/logger"
+
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+type Server struct{}
+
+func (cmd Server) Command(trap chan os.Signal) *cobra.Command {
+	run := func(_ *cobra.Command, _ []string) {
+		cmd.main(config.Load(true), trap)
+	}
+
+	return &cobra.Command{
+		Use:   "server",
+		Short: "run user server",
+		Run:   run,
+	}
+}
+
+func (cmd *Server) main(cfg *config.Config, trap chan os.Signal) {
+	logger := logger.NewZap(cfg.Logger)
+
+	// crypto := crypto.New(cfg.Crypto)
+	// token, err := token.New(cfg.Token)
+	// if err != nil {
+	// 	logger.Panic("Error creating token object", zap.Error(err))
+	// }
+
+	// go grpc.New(logger, crypto, token).Serve(9090)
+
+	// Keep this at the bottom of the main function
+	field := zap.String("signal trap", (<-trap).String())
+	logger.Info("exiting by receiving a unix signal", field)
+}
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/CafeKetab/auth-go/cmd"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+func main() {
+	const description = "user microservice for CafeKetab"
+	root := &cobra.Command{Short: description}
+
+	trap := make(chan os.Signal, 1)
+	signal.Notify(trap, syscall.SIGINT, syscall.SIGTERM)
+
+	root.AddCommand(
+		cmd.Server{}.Command(trap),
+	)
+
+	if err := root.Execute(); err != nil {
+		log.Fatal("failed to execute root command", zap.Error(err))
+	}
+}
