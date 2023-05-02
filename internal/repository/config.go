768b252cdb64efffb6b1f9df051ce5c23c1c2a2f
mohammadne <mohammadne.dev@gmail.com>
2023-05-02 07:15:47 +0330

    feat: remove redundant connfig file

--- internal/repository/config.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-type Config struct {
-	Host     string `koanf:"host"`
-	Port     int    `koanf:"port"`
-	Username string `koanf:"username"`
-	Password string `koanf:"password"`
-	Database string `koanf:"database"`
-}
